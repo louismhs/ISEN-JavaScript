2d831592870ec6a4b52c6c4aeffcfa3353097144
louismhs <louismilhes@hotmail.fr>
2020-03-20 14:33:18 +0100

    finish tp3

--- tp3/string.js
+++ tp3/string.js
@@ -1,3 +1,36 @@
 function repeat(s, n) {
     return s.repeat(n);
 }
+
+function truncate(s, n){
+    if (s.length <= n) {
+        return s
+    }
+    return s.slice(0, n) + '...'
+}
+
+function isPalindrome(x){
+    let y = (x.length);
+    let z = 0;
+    for (let i = 0; i < y / 2; i++) {
+        if (x[i] == x[y - i - 1]) {
+            z = z + 1;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+function swapCase(x){
+    let y = (x.length);
+    let z = "";
+    for (let i = 0; i < y; i++) {
+        if (x[i].charAt() <= 'Z' && x[i].charAt() >= 'A') {
+            z += x[i].toLowerCase();
+        } else {
+            z += x[i].toUpperCase();
+        }
+    }
+    return z;
+}
--- tp3/stringTest.js
+++ tp3/stringTest.js
@@ -1,3 +1,14 @@
 function repeatTests(){
     console.log(repeat("foo", 3)); // => foofoofoo
+
+    console.log(truncate("12345678912345", 20)); // => unchanged text
+    console.log(truncate("12345678912345", 14)); // => unchanged text
+    console.log(truncate("12345678912345", 10)); // => truncat...
+
+    console.log(isPalindrome("kayak"));         // => TRUE
+    console.log(isPalindrome("erlenmeyer"));    // => FALSE
+    console.log(isPalindrome("coloc"));         // => TRUE
+
+    console.log(swapCase("ErLeNmEyEr"));        // => eRlEnMeYeR
+    console.log(swapCase("HELLO WORLD !"));     // => hello world !
 }
