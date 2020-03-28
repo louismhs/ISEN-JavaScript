5eeaa634863cf6d2c522a91651127ea316a12349
louismhs <louismilhes@hotmail.fr>
2020-03-28 18:50:36 +0100

    add tp4

---
+++ tp4/tab.js
@@ -0,0 +1,58 @@
+function sum(numbers){
+    let sum = 0;
+
+    for (let number in numbers){
+        sum += numbers[number];
+    }
+    return sum;
+}
+
+function sum2(numbers){
+    let sum = 0;
+    let size = numbers.length;
+    
+    for (let i = 0; i < size; i++){
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+function longestWord(words){
+    let lword;
+    let longest = 0;
+    let size = words.length;
+
+    for (let i = 0; i < size; i++){
+        if (words[i].length > longest){
+            lword = words[i];
+            longest = words[i].length
+        }
+    }
+    return lword;
+}
+
+function range(min, max, step){
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i <= max){
+        if (min + step*j <= max){
+            result.push(min + step*j);
+        }
+        i = min + step*j;
+        j++;
+    }
+    return result;
+}
+
+function nbOccurences(wordList, word){
+    let tab = wordList.split(" ");
+    let size = wordList.length;
+    let occur = 0;
+    for (let i = 0; i < size; i++){
+        if (tab[i] == word){
+            occur++;
+        }
+    }
+    return occur;
+}
---
+++ tp4/tabTest.js
@@ -0,0 +1,18 @@
+function repeatTests(){
+
+    console.log(sum([]));               // => 0
+    console.log(sum([480,150,14,23]));  // => 667
+    console.log(sum2([]));              // => 0
+    console.log(sum2([480,150,14,23])); // => 667
+    console.log("------------------------------");
+    console.log(longestWord(["lot", "of", "words", "of", "different", "sizes"]));   // => "different"
+    console.log("------------------------------");
+    console.log(range(3, 8, 1));      // => [3, 4, 5, 6, 7, 8]
+    console.log(range(40, 90, 20));   // => [40, 60, 80]
+    console.log("------------------------------");
+    let text = "bar qux baz foo bar foo qux foo";
+    console.log(nbOccurences(text, "foo"));     // => 3
+    console.log(nbOccurences(text, "quux"));    // => 0
+    console.log(nbOccurences(text, "baz"));     // => 1
+
+}
