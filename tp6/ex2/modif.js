08d29443f02ba1d0797b07236c5fc4a4d2a43125
louismhs <louismilhes@hotmail.fr>
2020-05-06 10:51:19 +0200

    modif ex1 tp6 + debut ex2 tp6

---
+++ tp6/ex2/modif.js
@@ -0,0 +1,7 @@
+function main(){
+    
+    console.log("============================================");
+    console.log("Titre de la page : " + document.title);
+    console.log("Le nombre de ligne est : " + document.querySelectorAll("tr").length);
+
+}
