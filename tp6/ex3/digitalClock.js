6277155c6949dcad182085bb919bacaf9918cfcf
louismhs <louismilhes@hotmail.fr>
2020-05-13 10:10:26 +0200

    fin exo3 tp6

---
+++ tp6/ex3/digitalClock.js
@@ -0,0 +1,53 @@
+﻿function addSegments(digitId){
+    
+    let app = document.querySelector(digitId);
+    for(let i = 0; i < 7; i++){
+        app.innerHTML = app.innerHTML + "<div class=\"segment" + i + " segment off\"></div>";
+    }
+}
+
+function updateDigit(digitId, value){
+
+    let segmentStates = [
+        [1, 1, 1, 0, 1, 1, 1],     
+        [0, 0, 1, 0, 0, 1, 0],     
+        [1, 0, 1, 1, 1, 0, 1],     
+        [1, 0, 1, 1, 0, 1, 1],     
+        [0, 1, 1, 1, 0, 1, 0],     
+        [1, 1, 0, 1, 0, 1, 1],     
+        [1, 1, 0, 1, 1, 1, 1],     
+        [1, 0, 1, 0, 0, 1, 0],     
+        [1, 1, 1, 1, 1, 1, 1],      
+        [1, 1, 1, 1, 0, 1, 1]
+    ];
+
+    let app = document.querySelector(digitId);
+    for(let i = 0; i < 7; i++){
+        app.querySelector(".segment" + i).className = (segmentStates[value][i] == 1) ? "segment segment" + i : "segment segment" + i + " off";
+    }
+}
+
+
+
+
+function init(){
+    
+    addSegments("#hours-tens");
+    addSegments("#hours-units");
+    addSegments("#minutes-tens");
+    addSegments("#minutes-units");
+}
+
+
+
+function main(){
+
+    init();
+    updateDigit("#hours-tens", 1);
+    updateDigit("#hours-units", 2);
+    updateDigit("#minutes-tens", 3);
+    updateDigit("#minutes-units", 4);
+    
+}
+
+main();
